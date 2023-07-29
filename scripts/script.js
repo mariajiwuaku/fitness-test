375e16f8558f3ff67933204f6e3950ff2cea2202
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-07-29 08:34:20 -0700

    second commit

---
+++ scripts/script.js
@@ -0,0 +1,125 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "pink";
+}
+);
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "black";
+}
+);
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "black";
+}
+);
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) =>{
+    event.target.style.background = "white";
+    event.target.style.color = "black";
+}
+);
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) =>{
+    event.target.style.background = "red";
+    event.target.style.color = "black";
+}
+);
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) =>{
+    event.target.style.background = "purple";
+    event.target.style.color = "white";
+}
+);
+
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
+}
+);
+
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+}
+);
+
+const change9 = document.getElementById("image9");
+
+change9.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+}
+);
+
+const change10 = document.getElementById("image10");
+
+change10.addEventListener("click", (event) =>{
+    event.target.style.background = "red";
+    event.target.style.color = "black";
+}
+);
+
+const change11 = document.getElementById("image11");
+
+change11.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "black";
+}
+);
+
+const change12 = document.getElementById("image12");
+
+change12.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
+}
+);
+
+const change13 = document.getElementById("image13");
+
+change13.addEventListener("click", (event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "black";
+}
+);
+
+const change14 = document.getElementById("image14");
+
+change14.addEventListener("click", (event) =>{
+    event.target.style.background = "pink";
+    event.target.style.color = "black";
+}
+);
+
+const change15 = document.getElementById("image15");
+
+change15.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "black";
+}
+);const change16 = document.getElementById("image16");
+
+change16.addEventListener("click", (event) =>{
+    event.target.style.background = "brown";
+    event.target.style.color = "black";
+}
+);
