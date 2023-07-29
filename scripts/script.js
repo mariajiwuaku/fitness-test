dd663828f1b404546b51e8c97b1de887a9d3a528
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-07-29 09:58:32 -0700

    third commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const change = document.getElementById("image1");
 change.addEventListener("click", (event) =>{
     event.target.style.background = "blue";
     event.target.style.color = "pink";
+    change.textContent = "Image 1";
+
 }
 );
 
@@ -11,6 +13,8 @@ const change2 = document.getElementById("image2");
 change2.addEventListener("click", (event) =>{
     event.target.style.background = "green";
     event.target.style.color = "black";
+    change2.textContent = "Image 2";
+
 }
 );
 
@@ -19,6 +23,8 @@ const change3 = document.getElementById("image3");
 change3.addEventListener("click", (event) =>{
     event.target.style.background = "green";
     event.target.style.color = "black";
+    change3.textContent = "Image 3";
+
 }
 );
 
@@ -27,6 +33,8 @@ const change4 = document.getElementById("image4");
 change4.addEventListener("click", (event) =>{
     event.target.style.background = "white";
     event.target.style.color = "black";
+    change4.textContent = "Image 4";
+
 }
 );
 
@@ -35,6 +43,8 @@ const change5 = document.getElementById("image5");
 change5.addEventListener("click", (event) =>{
     event.target.style.background = "red";
     event.target.style.color = "black";
+    change5.textContent = "Image 5";
+
 }
 );
 
@@ -43,6 +53,8 @@ const change6 = document.getElementById("image6");
 change6.addEventListener("click", (event) =>{
     event.target.style.background = "purple";
     event.target.style.color = "white";
+    change6.textContent = "Image 6";
+
 }
 );
 
@@ -51,6 +63,8 @@ const change7 = document.getElementById("image7");
 change7.addEventListener("click", (event) =>{
     event.target.style.background = "yellow";
     event.target.style.color = "black";
+    change7.textContent = "Image 7";
+
 }
 );
 
@@ -59,6 +73,8 @@ const change8 = document.getElementById("image8");
 change8.addEventListener("click", (event) =>{
     event.target.style.background = "blue";
     event.target.style.color = "white";
+    change8.textContent = "Image 8";
+
 }
 );
 
@@ -67,6 +83,8 @@ const change9 = document.getElementById("image9");
 change9.addEventListener("click", (event) =>{
     event.target.style.background = "green";
     event.target.style.color = "white";
+    change9.textContent = "Image 9";
+
 }
 );
 
@@ -75,6 +93,8 @@ const change10 = document.getElementById("image10");
 change10.addEventListener("click", (event) =>{
     event.target.style.background = "red";
     event.target.style.color = "black";
+    change10.textContent = "Image 10";
+
 }
 );
 
@@ -83,6 +103,8 @@ const change11 = document.getElementById("image11");
 change11.addEventListener("click", (event) =>{
     event.target.style.background = "green";
     event.target.style.color = "black";
+    change11.textContent = "Image 11";
+
 }
 );
 
@@ -91,6 +113,8 @@ const change12 = document.getElementById("image12");
 change12.addEventListener("click", (event) =>{
     event.target.style.background = "yellow";
     event.target.style.color = "black";
+    change12.textContent = "Image 12";
+
 }
 );
 
@@ -99,6 +123,8 @@ const change13 = document.getElementById("image13");
 change13.addEventListener("click", (event) =>{
     event.target.style.background = "blue";
     event.target.style.color = "black";
+    change13.textContent = "Image 13";
+
 }
 );
 
@@ -107,6 +133,8 @@ const change14 = document.getElementById("image14");
 change14.addEventListener("click", (event) =>{
     event.target.style.background = "pink";
     event.target.style.color = "black";
+    change14.textContent = "Image 14";
+
 }
 );
 
@@ -115,11 +143,14 @@ const change15 = document.getElementById("image15");
 change15.addEventListener("click", (event) =>{
     event.target.style.background = "green";
     event.target.style.color = "black";
+    change15.textContent = "Image 15";
+
 }
 );const change16 = document.getElementById("image16");
 
 change16.addEventListener("click", (event) =>{
     event.target.style.background = "brown";
     event.target.style.color = "black";
+    change16.textContent = "Image 16";
 }
 );
